a4efbdbc8c7bb9360c5ff52953bf23806b2c9690
A01745656 <a01745656@itesm.mx>
2020-10-29 15:54:54 -0600

    Agrego main

---
+++ imagenmain.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+import black_white 
+import grayescale 
+import padding
+import convolucion
+imagen =cv2.imread("imagen.jpg")
+imagen_gray=grayescale.escala(imagen)
+valor = 127
+cv2.imwrite("grayScale.jpg", imagen_gray)
+imagen_black_white = black_white.black_white(imagen_gray, valor)
+cv2.imwrite("BlackWhite.jpg", imagen_black_white)
+filtro = [[3,4,2],[1,0,1],[2,3,1]]
+B = np.array(filtro)
+imagen_convolucion = convolucion.convolucion(imagen_gray,B)
+cv2.imwrite("ImagenConvolucion",imagen_convolucion)
+imagen_padd = padding.padding(imagen_gray)
+imagen_convolucion_padding = convolucion.convolucion(imagen_padd,B)
+cv2.imwrite("ImagenConvolucionPadding",imagen_convolucion_padding)
+
+
