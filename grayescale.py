ed6b01ad1d6cc852f254a946c8ddbc88b940fda2
a01745865 <69483330+a01745865@users.noreply.github.com>
2020-10-29 14:37:28 -0600

    Escala de grises

--- grayescale.py
+++ grayescale.py
@@ -20,6 +20,19 @@ def escala(M):
             matriz_zeros[i][a] = promedio 
     return matriz_zeros
 
+def black_white(M, valor):
+    matriz_zeros = np.zeros((M.shape[0], M.shape[1]))
+    for i in range(len(M)):
+        for a in range(len(M[i])):
+            if M[i][a] < valor:
+                matriz_zeros[i][a] = 0
+            elif M[i][a] >= valor:
+                matriz_zeros[i][a] = 255
+    return matriz_zeros
+
 imagen = cv2.imread("imagen.jpg")
 imagen_gray = escala(imagen)
 cv2.imwrite("GrayScale.jpg", imagen_gray)
+valor = 127
+imagen_black_white = black_white(imagen_gray, valor)
+cv2.imwrite("Blak&White.jpg", imagen_black_white)
