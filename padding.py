79b2fb4f049995e75eae565ff819bd8aecd61bb7
AriandaHCTec <A01749161@itesm.mx>
2020-10-29 15:28:25 -0600

    convolucion2

--- padding.py
+++ padding.py
@@ -6,9 +6,12 @@ Created on Thu Oct 29 14:45:35 2020
 @author: fergarcia
 """
 
+import numpy as np
+
 def padding(A):
+    A = A.tolist()
     D = []
     for i in A:
         D.append([0]+i+[0])
     ceros = [0]*(len(A[0])+2)
-    return [ceros]+D+[ceros]
+    return np.array([ceros]+D+[ceros])
