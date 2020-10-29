d58072bdfca8c1a5ab21ac4b7142c81e2f9bcadd
A01745656 <a01745656@itesm.mx>
2020-10-29 16:55:41 -0600

    Main final

--- imagenmain.py
+++ imagenmain.py
@@ -16,12 +16,9 @@ cv2.imwrite("BlackWhite.jpg", imagen_black_white)
 filtro = [[3,4,2],[1,0,1],[2,3,1]]
 B = np.array(filtro)
 imagen_convolucion = convolucion.convolucion(imagen_gray,B)
-<<<<<<< HEAD
-cv2.imwrite("ImagenConvolucion.jpg",imagen_convolucion)
-=======
-cv2.imwrite("ImagenConvolucion",imagen_convolucion)
 
->>>>>>> b9f105b71813bf967ae8866b8ecce5ad7fb0e741
+cv2.imwrite("ImagenConvolucion.jpg",imagen_convolucion)
+
 imagen_padd = padding.padding(imagen_gray)
 imagen_convolucion_padding = convolucion.convolucion(imagen_padd,B)
 cv2.imwrite("ImagenConvolucionPadding.jpg",imagen_convolucion_padding)
