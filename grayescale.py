413db30c155aabcbbe78b601b1274a53098d9126
a01745865 <69483330+a01745865@users.noreply.github.com>
2020-10-30 11:51:00 -0600

    Escala de grises

--- grayescale.py
+++ grayescale.py
@@ -24,14 +24,3 @@ def escala(M):
             matriz_zeros[i][a] = promedio 
     return matriz_zeros
 
-"""
-Filtro = [[3,4,2],[1,0,2],[2,3,1]]
-imagen = cv2.imread("imagen.jpg")
-imagen_gray = grayescale.escala(imagen)
-cv2.imwrite("GrayScale.jpg", imagen_gray)
-valor = 127
-imagen_black_white = black_white(imagen_gray, valor)
-cv2.imwrite("Blak&White.jpg", imagen_black_white)
-
-convolucion_sin = convolucion(imagen_gray, Filtro)
-"""
